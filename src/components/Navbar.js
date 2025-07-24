640c380f7db1fa66f1b2ba0d709ca434beda0bee
anushree410 <anudeota@gmail.com>
2025-07-24 22:50:13 +0530

    logo routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -37,8 +37,10 @@ function NavBar() {
       className={navColour ? "sticky" : "navbar"}
     >
       <Container>
-        <Navbar.Brand href="/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" />
+        <Navbar.Brand className="d-flex">
+          <Nav.Link as={Link} to="/" >
+            <img src={logo} className="img-fluid logo" alt="brand" />
+          </Nav.Link>
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
