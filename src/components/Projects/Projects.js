bb0c7e5a399ff87d51205d696f2da420c9116685
anushree410 <anudeota@gmail.com>
2025-08-06 01:08:10 +0530

    project images

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,13 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-//import leaf from "../../Assets/Projects/leaf.png";
-//import emotion from "../../Assets/Projects/emotion.png";
-//import editor from "../../Assets/Projects/codeEditor.png";
-//import chatify from "../../Assets/Projects/chatify.png";
-//import suicide from "../../Assets/Projects/suicide.png";
-//import bitsOfCode from "../../Assets/Projects/blog.png";
-
+import tumour from "../../Assets/Brain_Tumour.png";
+import game from "../../Assets/Destroy_Target.png";
+import backend from "../../Assets/img.png";
 function Projects() {
 
   return (
@@ -22,9 +18,20 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+        <Col md={4} className="project-card">
+                              <ProjectCard
+                                imgPath={backend}
+                                isBlog={false}
+                                title="Backend Django Application"
+                                description="This Project uses Django and Swagger-UI for RESTful APIs deployed using Render"
+                                ghLink="https://github.com/anushree410/django_project"
+                                demoLink="https://django-project-ekwx.onrender.com/docs/"
+                              />
+                </Col>
+
           <Col md={4} className="project-card">
             <ProjectCard
-//              imgPath={chatify}
+            imgPath={game}
               isBlog={false}
               title="Destroy target (game)"
               description="This project uses OpenCV in Python to identify hand gestures to destroy virtual targets on the screen."
@@ -34,13 +41,14 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-//              imgPath={bitsOfCode}
+              imgPath={tumour}
               isBlog={false}
               title="Brain Tumour Detection Using Image Processing"
               description="This Project uses MATLAB image processing to detect and outline Brain Tumour from MRI images if it exists."
               ghLink="https://github.com/anushree410/Brain-Tumor-Detection-using-Image-Processing"
             />
           </Col>
+
 
 
 
